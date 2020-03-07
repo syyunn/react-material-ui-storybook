151eed7876f8a61d607cc477f69d36a588569f89
zachary <syyun@snu.ac.kr>
2020-03-07 12:17:03 +0900

    Add hello-world button w. material ui

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+export default {
+  title: 'Button',
+};
+
+export const App = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+);
